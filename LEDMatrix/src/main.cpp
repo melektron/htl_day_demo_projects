bca5215132875c9dca91776171a3d6f532d02d4b
melektron <matteo@elektron.work>
2022-10-10 14:39:34 +0200

    added test code

--- LEDMatrix/src/main.cpp
+++ LEDMatrix/src/main.cpp
@@ -1,9 +1,31 @@
+/*
+ELEKTRON © 2022
+Written by melektron and MrFlunter
+www.elektron.work
+10.10.22, 14:16
+
+Arduino entry point for led display
+*/
+
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#include <FastLED.h>
+
+#define NUM_LEDS 60
+
+CRGB leds[NUM_LEDS];
+
+void setup()
+{
+    FastLED.addLeds<NEOPIXEL, 16>(leds, NUM_LEDS);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+    leds[0] = CRGB::White;
+    FastLED.show();
+    delay(30);
+    leds[0] = CRGB::Black;
+    FastLED.show();
+    delay(30);
 }
