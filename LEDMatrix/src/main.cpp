a42dbd73f50b11e20595dfc80a16c613bc8a6111
MrFlunter <flunterpertinger@tsn.at>
2022-10-12 10:36:50 +0200

    worked on animations

--- LEDMatrix/src/main.cpp
+++ LEDMatrix/src/main.cpp
@@ -18,63 +18,217 @@ matrixdriver<24, 8, 5> matrix;
 #define BLUE    0b0000000000011111
 
 
-#define MSPF 60 // ms per frame
 
+#define MSPF 180 // ms per frame
 
-void setup()
+void blackscreen()
 {
-    Serial.begin(9600);
-    
-    matrix.begin();
-    matrix.set_brightness(1);
-    matrix.set_mirror(true, false);
-
+    matrix.fillScreen(0);
 }
 
-void loop()
+void scroll_text(const char *_text, int16_t _x, int16_t _y)
 {
-    for (int i = 3; i < 21; i++)
+    // get text length
+    uint16_t iters;
+    int16_t grb;
+    uint16_t ugrb;
+    matrix.getTextBounds(_text, _x, _y, &grb, &grb, &iters, &ugrb);
+    matrix.setTextColor(BLUE);
+
+    // scroll text all the way off the screen (text length + screen width (= 24 px))
+    for (uint16_t i = 0; i < iters + 24; i++)
     {
-        matrix.drawCircle(i, 3, 4, RED);
+        matrix.setCursor(_x - i, _y);
+        matrix.print(_text);
+        matrix.update(); 
+        delay(MSPF);
+        matrix.fillScreen(0);
+    }
+}
+
+void stationary_circle()
+{
+    matrix.drawCircle(17, 4, 3, RED);
+    matrix.update();
+    
+    matrix.drawCircle(6, 4, 3, GREEN);
+    matrix.update();
+}
+    
+void smiley()
+{   
+    for (int i = 6; i < 20; i+=11)
+    {
+        matrix.drawCircle(i, 4, 3, RED);
+        matrix.update();
+        matrix.drawPixel(i-1, 3, BLUE);
+        matrix.drawPixel(i+1, 3, BLUE);
+        matrix.drawLine(i-1, 5, i+1, 5, GREEN);
+    }
+        
+    
+}
+
+void moving_circle()
+{
+    for (int i = 3; i < 21; i++)        //for (int i = 19; i > 3; i--)  -->  other direction 
+    {
+        matrix.drawCircle(i, 4, 3, RED);
         matrix.update();
         delay(MSPF);
-        matrix.drawCircle(i, 3, 4, 0);
+        matrix.drawCircle(i, 4, 3, 0);
         matrix.update();
     }
-    for (int i = 19; i > 3; i--)
-    {
-        matrix.drawCircle(i, 3, 5, GREEN);
-        matrix.update();
-        delay(MSPF);
-        matrix.drawCircle(i, 3, 5, 0);
-        matrix.update();
-    }
+}
+    
+void creating_sinus()
+{
     for (int i = 0; i < 24; i++)
     {
         matrix.drawPixel(i, round(3 + 2*sin(i)), GREEN);
         matrix.update();
         delay(MSPF);
     }
-    matrix.fillScreen(0);
-    matrix.setTextColor(BLUE);
+}
+
+void snake_line()
+{
+    int y = 1;
+    for (int x = 0; x < 23; x++)
+    {
+        matrix.drawPixel(x, y, GREEN);
+        matrix.update();
+        delay(MSPF);
+    }
+    matrix.drawPixel(22, y+1, GREEN);
+    matrix.update();
+    delay(MSPF);
+    y+=2;
+
+    for (int x = 22; x > 1; x--)
+    {
+        matrix.drawPixel(x, y, GREEN);
+        matrix.update();
+        delay(MSPF);
+    }
+    matrix.drawPixel(2, y+1, GREEN);
+    matrix.update();
+    delay(MSPF);
+    y+=2;
+
+    for (int x = 2; x < 23; x++)
+    {
+        matrix.drawPixel(x, y, GREEN);
+        matrix.update();
+        delay(MSPF);
+    }
+    matrix.drawPixel(22, y+1, GREEN);
+    matrix.update();
+    delay(MSPF);
+    y+=2;
+
+    for (int x = 22; x > -1; x--)
+    {
+        matrix.drawPixel(x, y, GREEN);
+        matrix.update();
+        delay(MSPF);
+    }
+    
+    for (int i = y; i > 0; i--)
+    {
+        matrix.drawPixel(0, i, GREEN);
+        matrix.update();
+        delay(MSPF);
+    }
+        
+}
+
+void reverse_snake_line()
+{
+    int y = 1;
+    for (int x = 0; x < 23; x++)
+    {
+        matrix.drawPixel(x, y, 0);
+        matrix.update();
+        delay(MSPF/2);
+    }
+    matrix.drawPixel(22, y+1, 0);
+    matrix.update();
+    delay(MSPF/2);
+    y+=2;
+
+    for (int x = 22; x > 1; x--)
+    {
+        matrix.drawPixel(x, y, 0);
+        matrix.update();
+        delay(MSPF/2);
+    }
+    matrix.drawPixel(2, y+1, 0);
+    matrix.update();
+    delay(MSPF/2);
+    y+=2;
+
+    for (int x = 2; x < 23; x++)
+    {
+        matrix.drawPixel(x, y, 0);
+        matrix.update();
+        delay(MSPF/2);
+    }
+    matrix.drawPixel(22, y+1, 0);
+    matrix.update();
+    delay(MSPF/2);
+    y+=2;
+
+    for (int x = 22; x > -1; x--)
+    {
+        matrix.drawPixel(x, y, 0);
+        matrix.update();
+        delay(MSPF/2);
+    }
+    
+    for (int i = y; i > 0; i--)
+    {
+        matrix.drawPixel(0, i, 0);
+        matrix.update();
+        delay(MSPF/2);
+    }
+        
+}
+
+void triangle()
+{   
+    int x1 = random(0,24);
+    int x2 = random(0,24);
+    int x3 = random(0,24);
+
+    int y1 = random(0,8);
+    int y2 = random(0,8);
+    int y3 = random(0,8);
+    for (int i = 0; i < 3; i++)
+    {
+        matrix.drawTriangle(x1,y1,x2,y2,x3,y3, random16(-1));
+        matrix.update();
+        delay(MSPF*10);
+    }
+    
+}
+
+void setup()
+{
+    Serial.begin(9600);
+    
+    matrix.begin();
+    matrix.set_brightness(5);
+    matrix.set_mirror(true, false);
     matrix.setTextSize(1);
     matrix.setTextWrap(0);
-    static const char* text_to_show = "HTL-Tage! Yeah! HTL! Come and remove the word 'Freetime' from your brain.";
 
-    // get text length
-    uint16_t iters;
-    int16_t grb;
-    uint16_t ugrb;
-    matrix.getTextBounds(text_to_show, 0, 0, &grb, &grb, &iters, &ugrb);
+}
 
-    // scroll text all the way off the screen (text length + screen width (= 24 px))
-    for (uint16_t i = 0; i < iters + 24; i++)
-    {
-        matrix.setCursor(0 - i, 0);
-        matrix.print(text_to_show);
-        matrix.update(); 
-        delay(MSPF);
-        matrix.fillScreen(0);
-    }
-    delay(500);
+
+
+void loop()
+{
+    triangle();
+    
 }
