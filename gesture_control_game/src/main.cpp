8f6b71aadf47a158f4497b899e543e96d9eea6b7
MrFlunter <flunterpertinger@tsn.at>
2023-10-04 14:35:23 +0200

    added code

--- gesture_control_game/src/main.cpp
+++ gesture_control_game/src/main.cpp
@@ -1,88 +1,115 @@
-#include <Arduino.h> 
+#include <Arduino.h>
 
-#define STEPPER_PIN_1 26 
+#define STEPPER_PIN_1 26
 #define STEPPER_PIN_2 25
 #define STEPPER_PIN_3 33
-#define STEPPER_PIN_4 32  
+#define STEPPER_PIN_4 32
+#define TRIGGER 23
+#define ECHO 22
 
+#define FULL_TURN 100
 int step_number = 0;
 
-void setup() {
-pinMode(STEPPER_PIN_1, OUTPUT);
-pinMode(STEPPER_PIN_2, OUTPUT);
-pinMode(STEPPER_PIN_3, OUTPUT);
-pinMode(STEPPER_PIN_4, OUTPUT);
+void setup()
+{
+    pinMode(STEPPER_PIN_1, OUTPUT);
+    pinMode(STEPPER_PIN_2, OUTPUT);
+    pinMode(STEPPER_PIN_3, OUTPUT);
+    pinMode(STEPPER_PIN_4, OUTPUT);
+    pinMode(TRIGGER, OUTPUT);
+    pinMode(ECHO, INPUT);
 }
 
-void OneStep(bool dir){
-    if(dir){
-switch(step_number){
-  case 0:
-  digitalWrite(STEPPER_PIN_1, HIGH);
-  digitalWrite(STEPPER_PIN_2, LOW);
-  digitalWrite(STEPPER_PIN_3, LOW);
-  digitalWrite(STEPPER_PIN_4, LOW);
-  break;
-  case 1:
-  digitalWrite(STEPPER_PIN_1, LOW);
-  digitalWrite(STEPPER_PIN_2, HIGH);
-  digitalWrite(STEPPER_PIN_3, LOW);
-  digitalWrite(STEPPER_PIN_4, LOW);
-  break;
-  case 2:
-  digitalWrite(STEPPER_PIN_1, LOW);
-  digitalWrite(STEPPER_PIN_2, LOW);
-  digitalWrite(STEPPER_PIN_3, HIGH);
-  digitalWrite(STEPPER_PIN_4, LOW);
-  break;
-  case 3:
-  digitalWrite(STEPPER_PIN_1, LOW);
-  digitalWrite(STEPPER_PIN_2, LOW);
-  digitalWrite(STEPPER_PIN_3, LOW);
-  digitalWrite(STEPPER_PIN_4, HIGH);
-  break;
-} 
-  }else{
-    switch(step_number){
-  case 0:
-  digitalWrite(STEPPER_PIN_1, LOW);
-  digitalWrite(STEPPER_PIN_2, LOW);
-  digitalWrite(STEPPER_PIN_3, LOW);
-  digitalWrite(STEPPER_PIN_4, HIGH);
-  break;
-  case 1:
-  digitalWrite(STEPPER_PIN_1, LOW);
-  digitalWrite(STEPPER_PIN_2, LOW);
-  digitalWrite(STEPPER_PIN_3, HIGH);
-  digitalWrite(STEPPER_PIN_4, LOW);
-  break;
-  case 2:
-  digitalWrite(STEPPER_PIN_1, LOW);
-  digitalWrite(STEPPER_PIN_2, HIGH);
-  digitalWrite(STEPPER_PIN_3, LOW);
-  digitalWrite(STEPPER_PIN_4, LOW);
-  break;
-  case 3:
-  digitalWrite(STEPPER_PIN_1, HIGH);
-  digitalWrite(STEPPER_PIN_2, LOW);
-  digitalWrite(STEPPER_PIN_3, LOW);
-  digitalWrite(STEPPER_PIN_4, LOW);
- 
-  
-} 
-  }
-step_number++;
-  if(step_number > 3){
-    step_number = 0;
-  }
+void OneStep(bool dir)
+{
+    if (dir)
+    {
+        switch (step_number)
+        {
+        case 0:
+            digitalWrite(STEPPER_PIN_1, HIGH);
+            digitalWrite(STEPPER_PIN_2, LOW);
+            digitalWrite(STEPPER_PIN_3, LOW);
+            digitalWrite(STEPPER_PIN_4, LOW);
+            break;
+        case 1:
+            digitalWrite(STEPPER_PIN_1, LOW);
+            digitalWrite(STEPPER_PIN_2, HIGH);
+            digitalWrite(STEPPER_PIN_3, LOW);
+            digitalWrite(STEPPER_PIN_4, LOW);
+            break;
+        case 2:
+            digitalWrite(STEPPER_PIN_1, LOW);
+            digitalWrite(STEPPER_PIN_2, LOW);
+            digitalWrite(STEPPER_PIN_3, HIGH);
+            digitalWrite(STEPPER_PIN_4, LOW);
+            break;
+        case 3:
+            digitalWrite(STEPPER_PIN_1, LOW);
+            digitalWrite(STEPPER_PIN_2, LOW);
+            digitalWrite(STEPPER_PIN_3, LOW);
+            digitalWrite(STEPPER_PIN_4, HIGH);
+            break;
+        }
+    }
+    else
+    {
+        switch (step_number)
+        {
+        case 0:
+            digitalWrite(STEPPER_PIN_1, LOW);
+            digitalWrite(STEPPER_PIN_2, LOW);
+            digitalWrite(STEPPER_PIN_3, LOW);
+            digitalWrite(STEPPER_PIN_4, HIGH);
+            break;
+        case 1:
+            digitalWrite(STEPPER_PIN_1, LOW);
+            digitalWrite(STEPPER_PIN_2, LOW);
+            digitalWrite(STEPPER_PIN_3, HIGH);
+            digitalWrite(STEPPER_PIN_4, LOW);
+            break;
+        case 2:
+            digitalWrite(STEPPER_PIN_1, LOW);
+            digitalWrite(STEPPER_PIN_2, HIGH);
+            digitalWrite(STEPPER_PIN_3, LOW);
+            digitalWrite(STEPPER_PIN_4, LOW);
+            break;
+        case 3:
+            digitalWrite(STEPPER_PIN_1, HIGH);
+            digitalWrite(STEPPER_PIN_2, LOW);
+            digitalWrite(STEPPER_PIN_3, LOW);
+            digitalWrite(STEPPER_PIN_4, LOW);
+        }
+    }
+    step_number++;
+    if (step_number > 3)
+    {
+        step_number = 0;
+    }
 }
 
-void loop() {
- 
-  OneStep(false);
-  delay(2);
-  
-
+double distance()
+{
+    digitalWrite(TRIGGER, HIGH);
+    delayMicroseconds(3);
+    digitalWrite(TRIGGER, LOW);
+    int duration = pulseIn(ECHO, HIGH);
+    double distance = duration * 0.0344 / 2;
+    return distance;
 }
 
+void loop()
+{
 
+    double turn = distance();
+    printf("%f\n",turn);
+    if (turn >= 30)
+    {
+        for (int a = 0; a <= FULL_TURN; a++)
+        {
+            OneStep(true);
+            delay(2);
+        }
+    }
+    delay(1000);
+}
